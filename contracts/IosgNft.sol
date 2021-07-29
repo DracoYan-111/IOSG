4885339a5d4009356ade578aa7b3e17fd58f361e
29449 <2944972991@qq.com>
2021-07-29 11:36:02 +0800

    ERC721合约完成

--- contracts/IosgNft.sol
+++ contracts/IosgNft.sol
@@ -4,12 +4,26 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+/**
+* @title IOSG ERC721 token
+*/
 contract IOSGNft is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+    /**
+    * @dev 部署合约
+    * @param _name token名称
+    * @param _symbol token简称
+    */
+    constructor(string memory _name, string memory _symbol)
+    ERC721(_name, _symbol) {}
 
+    /**
+    * @dev 创建nft
+    * @param player 收款人地址
+    * @param tokenURI tokenURI
+    */
     function awardItem(address player, string memory tokenURI)
     public
     returns (uint256)
