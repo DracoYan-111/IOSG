50807b65aa160610e0eeb74a876f4e09fb540c15
29449 <2944972991@qq.com>
2021-07-29 11:40:12 +0800

    ERC20合约的修改

--- contracts/IosgNft.sol
+++ contracts/IosgNft.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+
 /**
 * @title IOSG ERC721 token
 */
