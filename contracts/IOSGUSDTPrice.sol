da0d07fa5f8f32524ae32e8b03e0bf4162989471
29449 <2944972991@qq.com>
2021-08-01 23:19:24 +0800

    test

--- contracts/IOSGUSDTPrice.sol
+++ contracts/IOSGUSDTPrice.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 /**
-* @title IOSG USDT price
+* @title IOSG USDT price 
 */
 contract USDTPrice {
 
